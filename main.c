94158264bf07f76964def99e88ea06778d486644
Doğu <104231154+dogualpay@users.noreply.github.com>
2023-08-03 10:35:20 +0300

    Update main.c

--- main.c
+++ main.c
@@ -9,6 +9,8 @@
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
 #include "nrf_log_default_backends.h"
+#include "H3LIS331DL.h"
+
 
 /* TWI instance ID. */
 #define TWI_INSTANCE_ID     0
@@ -17,7 +19,8 @@
 static volatile bool m_xfer_done = false;
 
 /* TWI instance. */
-static const nrf_drv_twi_t m_twi = NRF_DRV_TWI_INSTANCE(TWI_INSTANCE_ID);
+const nrf_drv_twi_t m_twi = NRF_DRV_TWI_INSTANCE(TWI_INSTANCE_ID);
+
 
 /**
  * @brief TWI events handler.
@@ -39,13 +42,13 @@ void twi_handler(nrf_drv_twi_evt_t const * p_event, void * p_context)
 }
 
 /**
- * @brief UART initialization.
+ * @brief TWI initialization.
  */
 void twi_init (void)
 {
     ret_code_t err_code;
 
-    const nrf_drv_twi_config_t twi_lm75b_config = {
+    const nrf_drv_twi_config_t twi_config = {
        .scl                = ARDUINO_SCL_PIN,
        .sda                = ARDUINO_SDA_PIN,
        .frequency          = NRF_DRV_TWI_FREQ_100K,
@@ -53,7 +56,7 @@ void twi_init (void)
        .clear_bus_init     = false
     };
 
-    err_code = nrf_drv_twi_init(&m_twi, &twi_lm75b_config, twi_handler, NULL);
+    err_code = nrf_drv_twi_init(&m_twi, &twi_config, twi_handler, NULL);
     APP_ERROR_CHECK(err_code);
 
     nrf_drv_twi_enable(&m_twi);
@@ -65,10 +68,12 @@ void twi_init (void)
  */
 int main(void)
 {
-    printf("\r\nTWI sensor example started.");
+    printf("\r\nProgram started.");
     twi_init();
 
-    while (true)
+    
+
+    while (true) // MAIN TASK, INFINITE LOOP
     {
         nrf_delay_ms(500);
 
@@ -77,7 +82,7 @@ int main(void)
             __WFE();
         }while (m_xfer_done == false);
 
-        // read_sensor_data here !
+        
     }
 }
 
